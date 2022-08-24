825e473305ff8b34291c937f0ae1c91d5ca98494
fazilbek <brigadkasinaa@gmail.com>
2022-08-24 22:19:37 +0600

    App 1

---
+++ app/src/main/java/com/example/myapplication1/ui/notifications/NotificationsFragment.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication1.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication1.base.BaseFragment
+import com.example.myapplication1.databinding.FragmentNotificationsBinding
+
+class NotificationsFragment : BaseFragment<FragmentNotificationsBinding>(FragmentNotificationsBinding::inflate) {
+    override fun setupUI() {
+        TODO("Not yet implemented")
+    }
+}
